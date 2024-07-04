974b6136cc34e0ceadb0404194578a423e0df255
Melvin M T <melvinthankachan101@gmail.com>
2024-07-04 14:39:09 +0530

    Queue Practiced

--- queues/queue.py
+++ queues/queue.py
@@ -32,23 +32,30 @@ class Node:
 
 
 class QueueLL:
-    def __init__(self):
+    def __init__(self, limit):
         self.head = None
         self.tail = None
         self.length = 0
-    
+        self.limit = limit
+
     def is_empty(self):
         return self.length == 0
 
+    def is_full(self):
+        return self.limit == self.length
+
     def enqueue(self, item):
+        if self.is_full():
+            raise ValueError("Queue is full")
         new_node = Node(item)
         if self.is_empty():
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
-    
+
     def dequeue(self):
         if self.is_empty():
             raise ValueError("Queue is empty")
@@ -61,14 +68,23 @@ class QueueLL:
         if self.is_empty():
             raise ValueError("Queue is empty")
         return self.head.data
-    
+
     def print_queue(self):
         current_node = self.head
         print("[", end="")
         while current_node is not None:
             if current_node.next is not None:
-                print(f"{current_node.data}, ")
+                print(f"{current_node.data}, ", end="")
             else:
-                print(f"{current_node.data}")
+                print(f"{current_node.data}", end="")
             current_node = current_node.next
         print("]")
+
+
+queue = QueueLL(5)
+queue.enqueue(5)
+queue.enqueue(10)
+queue.enqueue(3)
+queue.enqueue(20)
+print(queue.peek())
+queue.print_queue()
