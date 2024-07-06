cdc87ae3f5b53189ce410e47d670002f2d24a648
Melvin M T <melvinthankachan101@gmail.com>
2024-07-06 11:11:35 +0530

    Sorting practice

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-import 
+from queue import Queue
+
+q = Queue(maxsize=5)
+
+q.put(10, timeout=1)
+q.put(20, timeout=1)
+q.put(30, timeout=1)
+q.put(40, timeout=1)
+q.put(50, timeout=1)
+q.get(timeout=1)
+
+
+print(list(q.queue))
